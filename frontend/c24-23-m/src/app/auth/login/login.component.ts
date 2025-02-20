aad58417d05af295a8a89e763ed22b424a415352
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-02-19 23:19:36 -0300

    validacion de formularios y pequeñas correciones

--- frontend/c24-23-m/src/app/auth/login/login.component.ts
+++ frontend/c24-23-m/src/app/auth/login/login.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+  
+    constructor(private fb: FormBuilder) { 
+      this.loginForm = this.fb.group({
+        
+        email: ['', [Validators.required, Validators.email]],
+        password: ['',[Validators.required, Validators.minLength(8)]], //Validaciones del formnulario
+      })
+    }
+  
+  
+    login() {
+      if (this.loginForm.valid) {
+        console.log(this.loginForm.value);    //Aca se enviarian los datos al backend
+      }
+      else{
+        console.log("Formualrio Invalido")
+      }
+    }
 
 }
--- frontend/c24-23-m/src/app/auth/register/register.component.ts
+++ frontend/c24-23-m/src/app/auth/register/register.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  standalone: true, // ✅ Indica que es un componente independiente
-  imports: [FormsModule], // ✅ Importar FormsModule aquí
+  standalone: true, // Indica que es un componente independiente
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  userType: string = ''; // ✅ Variable para guardar el tipo de usuario
+
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.registerForm = this.fb.group({
+      
+      name: ['',[Validators.required]],
+      apellido: [],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]], //Validaciones del formnulario
+      userType: ['',[Validators.required]],
+      calle: ['',[Validators.required]],
+      enumeracion: ['',[Validators.required]],
+      telefono: ['',[Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);    //Aca se enviarian los datos al backend
+    }
+    else{
+      console.log("Formualrio Invalido")
+    }
+  }
+
 }
