61368eb32645313d27f3824ab7c9f6682f83014a
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-02-24 21:12:10 -0300

    more stash

--- frontend/c24-23-m/src/app/auth/login/login.component.ts
+++ frontend/c24-23-m/src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { AuthGoogleService } from '../../core/services/auth-google.service';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,32 @@ import { AuthGoogleService } from '../../core/services/auth-google.service';
 })
 export class LoginComponent {
 
-  constructor(private authGoogleService: AuthGoogleService) { }
-  login() {
+  constructor(private authGoogleService: AuthGoogleService,private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]], //Validaciones del formnulario
+    })
+   }
+
+
+  loginG() {
     this.authGoogleService.login();
+  }
+  loginForm!: FormGroup;
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);    //Aca se enviarian los datos al backend
+    }
+    else{
+      console.log("Formulario Invalido")
+    }
+  }
+  
+    
   }
 
 
-}
+
+
