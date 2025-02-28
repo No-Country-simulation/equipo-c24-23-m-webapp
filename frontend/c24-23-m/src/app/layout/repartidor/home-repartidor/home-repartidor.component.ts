220fef078a355d38517c624681fbf5d8707a7aef
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-02-28 01:38:07 -0300

    organizacion

---
+++ frontend/c24-23-m/src/app/layout/repartidor/home-repartidor/home-repartidor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-repartidor',
+  imports: [],
+  templateUrl: './home-repartidor.component.html',
+  styleUrl: './home-repartidor.component.css'
+})
+export class HomeRepartidorComponent {
+
+}
