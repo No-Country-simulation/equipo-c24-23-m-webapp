d10a8edcfc448a4d87d4e666361a79b7bf9b4ef4
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-03-03 13:23:46 -0300

    restaurantes-clientes terminado

--- frontend/c24-23-m/src/app/app.routes.ts
+++ frontend/c24-23-m/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { MainComponent } from './layout/main/main.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeClienteComponent } from './features/cliente/home-cliente/home-cliente.component';
+import { RestaurantesComponent } from './features/cliente/restaurantes/restaurantes.component';
+import { RestauranteMenuComponent } from './features/cliente/restaurante-menu/restaurante-menu.component';
 
 export const routes: Routes = [
   {path: '',  redirectTo: 'home-clientes', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'main', component: MainComponent},
-  {path: 'home-clientes', component: HomeClienteComponent}
+  {path: 'home-clientes', component: HomeClienteComponent},
+  {path: 'restaurantes-clientes', component:RestaurantesComponent},
+  {path: 'menu/:id', component: RestauranteMenuComponent }
 ];
--- frontend/c24-23-m/src/app/features/cliente/home-cliente/home-cliente.component.ts
+++ frontend/c24-23-m/src/app/features/cliente/home-cliente/home-cliente.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
+import { SlideRestaurantesComponent } from "../slide-restaurantes/slide-restaurantes.component";
 
 @Component({
   selector: 'app-home-cliente',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SlideRestaurantesComponent],
   templateUrl: './home-cliente.component.html',
   styleUrl: './home-cliente.component.css'
 })
@@ -32,30 +33,4 @@ export class HomeClienteComponent {
     return groups;
   }
 
-  // Datos de prueba
-  restaurants = [
-    { id: 1, name: 'Restaurante Italiano', image: 'https://via.placeholder.com/300x200' },
-    { id: 2, name: 'Sushi Express', image: 'https://via.placeholder.com/300x200' },
-    { id: 3, name: 'Asador Argentino', image: 'https://via.placeholder.com/300x200' },
-    { id: 4, name: 'Tacos Mexicanos', image: 'https://via.placeholder.com/300x200' },
-    { id: 5, name: 'Burger House', image: 'https://via.placeholder.com/300x200' },
-    { id: 6, name: 'Veggie Delight', image: 'https://via.placeholder.com/300x200' },
-    { id: 7, name: 'Gloton', image: 'https://via.placeholder.com/300x200' },
-    { id: 8, name: 'Argumentos', image: 'https://via.placeholder.com/300x200' }
-  ];
-
-  // Función para dividir los restaurantes en grupos de 3 por slide
-  getRestaurantGroups(): any[] {
-    const chunkSize = 4;
-    const groups = [];
-    for (let i = 0; i < this.restaurants.length; i += chunkSize) {
-      groups.push(this.restaurants.slice(i, i + chunkSize));
-    }
-    return groups;
-  }
-
-   // Navegar al menú del restaurante
-   goToMenu(restaurantId: number) {
-    this.router.navigate(['/menu', restaurantId]);
-  }
 }
